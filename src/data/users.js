e008e490c479c1fd46c6311b46937d06cb38a247
Fabio Mendes <fabiomendes53@gmail.com>
2022-03-02 11:28:21 -0300

    feat: create mock database for users

---
+++ src/data/users.js
@@ -0,0 +1,26 @@
+export default [
+    {
+        id: 1,
+        name: 'João Silva',
+        email: 'jo.sil@empmail.com',
+        avatarUrl: 'https://cdn.pixabay.com/photo/2016/07/22/16/54/portrait-1535266_960_720.jpg',
+    },
+    {
+        id: 2,
+        name: 'Mariana Aguiar',
+        email: 'mariana.a@empmail.com',
+        avatarUrl: 'https://cdn.pixabay.com/photo/2016/11/29/06/08/woman-1867715_960_720.jpg',
+    },
+    {
+        id: 3,
+        name: 'Julia Fragoso',
+        email: 'ju.fragoso@empmail.com',
+        avatarUrl: 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png',
+    },
+    {
+        id: 4,
+        name: 'Rafael Monteiro',
+        email: 'r.monteiro@empmail.com',
+        avatarUrl: 'https://cdn.pixabay.com/photo/2014/04/12/14/59/portrait-322470_960_720.jpg',
+    },
+];
