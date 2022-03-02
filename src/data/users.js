3197d151393908f2b78497c2fbcfb445b8da5c6f
Fabio Mendes <fabiomendes53@gmail.com>
2022-03-02 11:55:20 -0300

    refactor: update users profile picture

--- src/data/users.js
+++ src/data/users.js
@@ -3,24 +3,24 @@ export default [
         id: 1,
         name: 'João Silva',
         email: 'jo.sil@empmail.com',
-        avatarUrl: 'https://cdn.pixabay.com/photo/2016/07/22/16/54/portrait-1535266_960_720.jpg',
+        avatarUrl: 'https://cdn.pixabay.com/photo/2014/04/03/10/32/businessman-310819_960_720.png',
     },
     {
         id: 2,
         name: 'Mariana Aguiar',
         email: 'mariana.a@empmail.com',
-        avatarUrl: 'https://cdn.pixabay.com/photo/2016/11/29/06/08/woman-1867715_960_720.jpg',
+        avatarUrl: 'https://cdn.pixabay.com/photo/2014/04/03/10/32/user-310807_960_720.png',
     },
     {
         id: 3,
         name: 'Julia Fragoso',
         email: 'ju.fragoso@empmail.com',
-        avatarUrl: 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png',
+        avatarUrl: 'https://cdn.pixabay.com/photo/2014/03/24/17/19/teacher-295387_960_720.png',
     },
     {
         id: 4,
         name: 'Rafael Monteiro',
         email: 'r.monteiro@empmail.com',
-        avatarUrl: 'https://cdn.pixabay.com/photo/2014/04/12/14/59/portrait-322470_960_720.jpg',
+        avatarUrl: 'https://cdn.pixabay.com/photo/2016/04/01/11/25/avatar-1300331_960_720.png',
     },
 ];
