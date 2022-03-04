d51fca49d8ff4a905fd4fb60c027323534aa908b
Fabio Mendes <fabiomendes53@gmail.com>
2022-03-03 21:28:13 -0300

    feat: create form for user form screen

--- src/views/UserForm.js
+++ src/views/UserForm.js
@@ -1,8 +1,50 @@
-import React from 'react'
-import { Text } from 'react-native'
+import React, { useState } from 'react'
+import { Text, TextInput, View, StyleSheet, Button } from 'react-native'
 
-export default props => {
+export default ({route, navigation}) => {
+    const [user, setUser] = useState(route.params ? route.params : {})
     return (
-        <Text>UserForm</Text>
+        <View style={style.form}>
+            <Text>Nome:</Text>
+            <TextInput 
+                style={style.input}
+                onChangeText={name => setUser({...user, name})}
+                placeholder=" Digite..."
+                value={user.name}
+            />
+            <Text>Email:</Text>
+            <TextInput 
+                style={style.input}
+                onChangeText={email => setUser({...user, email})}
+                placeholder=" Digite..."
+                value={user.email}
+            />
+            <Text>Link da foto de perfil:</Text>
+            <TextInput 
+                style={style.input}
+                onChangeText={avatarUrl => setUser({...user, avatarUrl})}
+                placeholder=" Digite..."
+                value={user.avatarUrl}
+            />
+            <Button 
+                title="Salvar"
+                onPress={() => {
+                    navigation.goBack()
+                }}
+            />
+        </View>
     )
 }
+
+const style = StyleSheet.create({
+    form: {
+        padding: 12,
+    },
+    input: {
+        height: 40,
+        borderColor: 'gray',
+        borderWidth: 1,
+        marginBottom: 10,
+        borderRadius: 15,
+    },
+})
