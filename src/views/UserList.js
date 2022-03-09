580146a4262b5b1765888b538b2e536f5797fabd
Fabio Mendes <fabiomendes53@gmail.com>
2022-03-09 20:09:03 -0300

    refactor: change dispatch type name

--- src/views/UserList.js
+++ src/views/UserList.js
@@ -13,7 +13,7 @@ export default props => {
                 text: 'Sim',
                 onPress() {
                     dispatch({
-                        type: "DeleteUser",
+                        type: "deleteUser",
                         payload: user,
                     })
                 }
